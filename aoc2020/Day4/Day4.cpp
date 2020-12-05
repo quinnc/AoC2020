fb6bd55dc607b8ee62dea9136ae6a20088c9edcb
quinnc <q.liesch+git@gmail.com>
2020-12-05 16:22:45 -0700

    day 4 part 1 complete

--- aoc2020/Day4/Day4.cpp
+++ aoc2020/Day4/Day4.cpp
@@ -6,9 +6,14 @@
 #include <string>
 #include <fstream>
 
+//#include <boost/algorithm>
+#include <boost/algorithm/string.hpp> 
+
 #include "FileLoad.h"
 
+
 using namespace std;
+using namespace boost::algorithm;
 
 void Day4Part1(vector<string>& lines);
 
@@ -33,7 +38,86 @@ int main(int argc, char** argv)
 	return 0;
 }
 
+#define SIZEOF(x) (sizeof(x)/sizeof(x[0]))
+const char* keys[] = { "byr", "iyr", "eyr", "hgt","hcl", "ecl", "pid", "cid" };
+
 void Day4Part1(vector<string>& lines)
 {
 
+	bool foundKeys[SIZEOF(keys)] = { false, false, false, false, false, false, false };
+	int validPassports = 0;
+	int invalidPassports = 0;
+
+	cout << "number of lines: " << lines.size() << endl;
+	cout << "size of keys = " << SIZEOF(keys) << endl;
+
+	for (int i = 0; i < lines.size(); i++)
+	{
+		cout << i << ": >>" << lines[i] << "<<" << endl;
+		
+		
+		std::string trimmed = boost::algorithm::trim_copy(lines[i]);
+		if (trimmed.empty())
+		{
+			bool foundAllButCid = true;
+			// check the state, then clear it
+			for (auto kIndex = 0; kIndex < SIZEOF(keys); kIndex++)
+			{
+				if (strncmp(keys[kIndex], "cid", 3) != 0)
+				{
+					foundAllButCid &= foundKeys[kIndex];
+				}
+				foundKeys[kIndex] = false;
+			}
+
+
+			// log success or failure
+			if (foundAllButCid)
+				validPassports++;
+			else
+				invalidPassports++;
+
+
+			// start next
+			continue;
+		}
+
+		std::vector<std::string> results;
+		boost::split(results, trimmed, [](char c) {return c == ' ' || c == ':'; });
+
+		for (auto currPart = 0; currPart < results.size(); currPart += 2)
+		{
+			for (auto kIndex = 0; kIndex < SIZEOF(keys); kIndex++)
+			{
+				if (results[currPart] == keys[kIndex])
+				{
+					cout << "found part: " << keys[kIndex] << " mate:" << results[currPart + 1] << endl;
+					foundKeys[kIndex] = true;
+				}
+			}
+		}
+	}
+
+	//if (false)
+	{
+		bool foundAllButCid = true;
+		// check the state, then clear it
+		for (auto kIndex = 0; kIndex < SIZEOF(keys); kIndex++)
+		{
+			if (strncmp(keys[kIndex], "cid", 3) != 0)
+			{
+				foundAllButCid &= foundKeys[kIndex];
+			}
+			foundKeys[kIndex] = false;
+		}
+
+
+		// log success or failure
+		if (foundAllButCid)
+			validPassports++;
+		else
+			invalidPassports++;
+	}
+
+	cout << "Passports valid=" << validPassports << ", invalid=" << invalidPassports << endl;
 }
