a91a9c03c75c27f930195a92a1bcbd37d8cf8d22
quinnc <q.liesch+git@gmail.com>
2020-12-04 20:50:15 -0700

    Create shared code

---
+++ aoc2020/SharedCode/FileLoad.cpp
@@ -0,0 +1,43 @@
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+
+#include "FileLoad.h"
+
+using namespace std;
+
+
+
+bool OpenAndReadInput(char* filename, vector<string>& lines)
+{
+	fstream inputfile;
+	string line;
+
+	inputfile.open(filename, ios::in);
+
+	if (!inputfile.is_open())
+	{
+		cout << " Unable to open " << filename << endl;
+		return false;
+	}
+
+	while (getline(inputfile, line))
+	{
+		//int cost = std::stoi(line);
+		lines.push_back(line);
+	}
+
+	inputfile.close();
+	return true;
+}
+
+
+void ShowInputs(int argc, char** argv)
+{
+	std::cout << "Hello World! argc=" << argc << std::endl;
+	for (int i = 0; i < argc; i++)
+		std::cout << i << " : " << argv[i] << std::endl;
+}
+
---
+++ aoc2020/SharedCode/FileLoad.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <vector>
+#include <string>
+
+bool OpenAndReadInput(char* filename, std::vector<std::string>& lines);
+void ShowInputs(int argc, char** argv);
