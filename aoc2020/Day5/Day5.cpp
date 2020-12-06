ec8544dbe652c9b56b209de873e3a865ecc69583
quinnc <q.liesch+git@gmail.com>
2020-12-05 17:14:13 -0700

    Creation Day 5

---
+++ aoc2020/Day5/Day5.cpp
@@ -0,0 +1,28 @@
+// Day5.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+
+#include "FileLoad.h"
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	std::cout << "Hello World!\n";
+
+	ShowInputs(argc, argv);
+
+	if (argc < 2)
+		return -2;
+
+	vector<string> lines;
+	if (!OpenAndReadInput(argv[1], lines))
+	{
+		return -1;
+	}
+
+}
