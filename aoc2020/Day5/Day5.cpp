5eb5b99285bb1ad33c68f1bf472c70472ee96b06
quinnc <q.liesch+git@gmail.com>
2020-12-12 09:19:48 -0700

    Works with samples

--- aoc2020/Day5/Day5.cpp
+++ aoc2020/Day5/Day5.cpp
@@ -10,6 +10,8 @@
 
 using namespace std;
 
+int HighestId(vector<string>& lines);
+
 int main(int argc, char** argv)
 {
 	std::cout << "Hello World!\n";
@@ -25,4 +27,80 @@ int main(int argc, char** argv)
 		return -1;
 	}
 
+	cout << "Highest seat id = " << HighestId(lines) << endl;
 }
+
+int GetRow(string rowpartitions)
+{
+	cout << " Interpretting row: " << rowpartitions << endl;
+	int currMin = 0;
+	int currMax = 127;
+	int middle = currMax / 2;
+
+	for (size_t i = 0; i < rowpartitions.length(); i++)
+	{
+		if (rowpartitions[i] == 'F')
+			currMax = middle;
+		else
+			currMin = middle + 1;
+
+		middle = (currMin + currMax) / 2;
+		cout << "i=" << i << "; char=" << rowpartitions[i] << "; Min=" << currMin << "; Max=" << currMax << "; middle=" << middle << endl;
+
+	}
+
+	cout << " Min=" << currMin << "; Max=" << currMax << "; middle=" << middle << endl;
+
+	return middle;
+}
+
+
+int GetColumn(string colPartitions)
+{
+	cout << " Interpretting columns: " << colPartitions << endl;
+
+	int currMin = 0;
+	int currMax = 7;
+	int middle = currMax / 2;
+
+	for (size_t i = 0; i < colPartitions.length(); i++)
+	{
+		if (colPartitions[i] == 'L')
+			currMax = middle;
+		else
+			currMin = middle + 1;
+
+		middle = (currMin + currMax) / 2;
+		cout << "i=" << i << "; char=" << colPartitions[i] << "; Min=" << currMin << "; Max=" << currMax << "; middle=" << middle << endl;
+
+	}
+
+	cout << " Min=" << currMin << "; Max=" << currMax << "; middle=" << middle << endl;
+
+	return middle;
+}
+
+int SeatId(int col, int row)
+{
+
+	return col * 8 + row;
+}
+
+int HighestId(vector<string>& lines)
+{
+	int currentHigh = 0;
+
+	for (size_t i = 0; i < lines.size(); i++)
+	{
+		cout << "Current line == " << lines[i] << endl;
+		int row = GetRow(lines[i].substr(0, 7));
+		int col = GetColumn(lines[i].substr(7, 3));
+		int id = SeatId(row, col);
+
+		cout << "  Row =" << row << ", col=" << col << ", id =" << id << endl;
+		if (id > currentHigh)
+			currentHigh = id;
+	}
+	return currentHigh;
+}
+
