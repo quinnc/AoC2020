89744fa067ae3e59692da5aae3d13accd3522f5b
quinnc <q.liesch+git@gmail.com>
2020-12-13 15:47:42 -0700

    part B works with example code

--- aoc2020/Day8/Day8.cpp
+++ aoc2020/Day8/Day8.cpp
@@ -31,7 +31,7 @@ int main(int argc, char** argv)
 
 
 	cout << "PART A:::  accumlator = " << PartA(lines) << endl;
-	//cout << "PART B::: bags in shiny gold = " << PartB(lines) << endl;
+	cout << "PART B::: fixed code accumulator = " << PartB(lines) << endl;
 
 	return 0;
 
@@ -51,14 +51,83 @@ const char* const OpCodeStrs[] = {
 };
 
 
-int PartA(vector<string>& lines)
+OpCodes GetOpCode(string& line)
 {
-	int accum = 0;
-	int currLine = 0;
+	string opstr = line.substr(0, 3);
+
+	if (opstr == OpCodeStrs[0])
+		return OpCodes::acc;
+
+	if (opstr == OpCodeStrs[1])
+		return OpCodes::nop;
+
+	if (opstr == OpCodeStrs[2])
+		return OpCodes::jmp;
+
+	throw std::exception("unknown op code!");
+}
+
+void RunCode(vector<string>& code, bool& cleanFinish, bool& infinite, bool& inLalaland, int& accum)
+{
+	accum = 0;
+	size_t currLine = 0;
 	unordered_map<int, int> visitedLines;
 	string instr;
 
-	while (visitedLines.find(currLine) == visitedLines.end())
+	cleanFinish = false;
+	infinite = false;
+	inLalaland = false;
+
+
+	while (currLine >= 0 && currLine < code.size() && visitedLines.find(currLine) == visitedLines.end())
+	{
+		visitedLines.insert(make_pair(currLine, currLine));
+		instr = code[currLine].substr(0, 3);
+
+		//cout << "Current line: " << lines[currLine] << ", instruction= >>" << instr << "<<" << ", accumulator=" << accum << ", line num=" << currLine << endl;
+
+		OpCodes op = GetOpCode(code[currLine]);
+
+		switch (op)
+		{
+		case OpCodes::nop:
+			currLine++;
+			break;
+
+		case OpCodes::acc:
+		{
+			int val = stoi(code[currLine].substr(4));
+			accum += val;
+			currLine++;
+			break;
+		}
+		case OpCodes::jmp:
+		{
+			int val = stoi(code[currLine].substr(4));
+			currLine += val;
+			break;
+		}
+		}
+	}
+
+
+	inLalaland = (currLine < 0 || currLine > code.size());
+
+	cleanFinish = (currLine == code.size());
+
+	infinite = (currLine >= 0 && currLine < code.size());
+
+}
+
+
+int PartA(vector<string>& lines)
+{
+	int accum = 0;
+	size_t currLine = 0;
+	unordered_map<int, int> visitedLines;
+	string instr;
+
+	while (currLine < lines.size() && visitedLines.find(currLine) == visitedLines.end())
 	{
 		visitedLines.insert(make_pair(currLine, currLine));
 		instr = lines[currLine].substr(0, 3);
@@ -83,4 +152,50 @@ int PartA(vector<string>& lines)
 	}
 
 	return accum;
+}
+
+
+int PartB(vector<string>& lines)
+{
+	int totalLines = lines.size();
+	int curr;
+	string savedLine;
+	string newLine;
+
+	int accum;
+
+	for (curr = 0; curr < totalLines; curr++)
+	{
+		savedLine = lines[curr];
+
+
+		OpCodes op = GetOpCode(savedLine);
+
+		switch (op)
+		{
+		case OpCodes::jmp:
+			newLine = OpCodeStrs[(int)OpCodes::nop] + savedLine.substr(3);
+			break;
+
+		case OpCodes::nop:
+			newLine = OpCodeStrs[(int)OpCodes::jmp] + savedLine.substr(3);
+			break;
+
+		default:
+			// try another
+			continue;
+		}
+
+		lines[curr] = newLine;
+
+		bool clean, inf, lala;
+		RunCode(lines, clean, inf, lala, accum);
+
+		lines[curr] = savedLine;
+
+		if (clean)
+			return accum;
+	}
+
+	return -1;
 }
