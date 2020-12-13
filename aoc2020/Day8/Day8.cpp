83b3ca30a2eb746f14fe6b6b5ee0f46d845633d7
quinnc <q.liesch+git@gmail.com>
2020-12-13 15:17:09 -0700

    works for example code part A

--- aoc2020/Day8/Day8.cpp
+++ aoc2020/Day8/Day8.cpp
@@ -5,7 +5,7 @@
 #include <vector>
 #include <string>
 #include <fstream>
-#include <map>
+#include <unordered_map>
 
 #include "FileLoad.h"
 
@@ -30,10 +30,57 @@ int main(int argc, char** argv)
 	}
 
 
-	cout << "PART A::: number of suitcase that can contain shiny gold = " << PartA(lines) << endl;
-	cout << "PART B::: bags in shiny gold = " << PartB(lines) << endl;
+	cout << "PART A:::  accumlator = " << PartA(lines) << endl;
+	//cout << "PART B::: bags in shiny gold = " << PartB(lines) << endl;
 
 	return 0;
 
 }
 
+enum class OpCodes
+{
+	acc,
+	nop,
+	jmp
+};
+
+const char* const OpCodeStrs[] = {
+	"acc",
+	"nop",
+	"jmp",
+};
+
+
+int PartA(vector<string>& lines)
+{
+	int accum = 0;
+	int currLine = 0;
+	unordered_map<int, int> visitedLines;
+	string instr;
+
+	while (visitedLines.find(currLine) == visitedLines.end())
+	{
+		visitedLines.insert(make_pair(currLine, currLine));
+		instr = lines[currLine].substr(0, 3);
+
+		cout << "Current line: " << lines[currLine] << ", instruction= >>" << instr << "<<" << ", accumulator=" << accum << ", line num=" << currLine << endl;
+
+		if (instr == OpCodeStrs[(int)OpCodes::nop])
+		{
+			currLine++;
+		}
+		else if (instr == OpCodeStrs[(int)OpCodes::acc])
+		{
+			int val = stoi(lines[currLine].substr(4));
+			accum += val;
+			currLine++;
+		}
+		else if (instr == OpCodeStrs[(int)OpCodes::jmp])
+		{
+			int val = stoi(lines[currLine].substr(4));
+			currLine += val;
+		}
+	}
+
+	return accum;
+}
