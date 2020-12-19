346e3933246ffd612709f8b09916afd912b7544a
quinnc <q.liesch+git@gmail.com>
2020-12-19 15:11:39 -0700

    part b example 1 works

--- aoc2020/Day10/Day10.cpp
+++ aoc2020/Day10/Day10.cpp
@@ -14,7 +14,7 @@ using namespace std;
 
 
 int PartA(vector<string>& lines);
-int PartB(vector<string>& lines);
+unsigned long long int PartB(vector<string>& lines);
 
 
 int main(int argc, char** argv)
@@ -34,19 +34,27 @@ int main(int argc, char** argv)
 	}
 
 	cout << " Number of 1 jolts steps * Number of 3 jolt steps = " << PartA(lines) << endl;
+	cout << " Number of paths = " << PartB(lines) << endl;
 }
 
-int PartA(vector<string>& lines)
-{
-	vector<int> adapterJolts;
-	int result = -1;
 
+void PrepAdapters(const vector<string>& lines, vector<int>& adapterJolts)
+{
 	for (size_t i = 0; i < lines.size(); i++)
 	{
 		adapterJolts.push_back(stoi(lines[i]));
 	}
 
 	std::sort(adapterJolts.begin(), adapterJolts.end());
+}
+
+
+int PartA(vector<string>& lines)
+{
+	vector<int> adapterJolts;
+	int result = -1;
+
+	PrepAdapters(lines, adapterJolts);
 
 	// one 3 Jolt step for the last adapter to device jump //
 	int joltDiffs[5] = { 0,0,0,1,0 };
@@ -57,7 +65,7 @@ int PartA(vector<string>& lines)
 		int deltaJolt = adapterJolts[j] - prevJolt;
 		cout << "Diff is: " << deltaJolt << endl;
 
-		if (deltaJolt < 0 || deltaJolt > 4)
+		if (deltaJolt < 1 || deltaJolt > 4)
 		{
 			cout << "^^^ INVALID JOLT ****************************" << endl;
 			continue;
@@ -71,8 +79,66 @@ int PartA(vector<string>& lines)
 	return result;
 }
 
-
-int PartB(vector<string>& lines)
+void PDepth(int depth)
 {
-	return -1;
+	for (int d = 0; d < depth; d++)
+		cout << "\t";
+
+}
+
+// returns the number of paths found
+unsigned long long  int FindPaths(const vector<int>& adapterJolts, int startIndex, int startJolt, int targetJolt)
+{
+	unsigned long long int pathsHere = 0;
+
+	static int depth = 1;
+	
+	
+	//PDepth(depth);
+	//cout << " i=" << startIndex << " start J=" << startJolt << endl;
+
+	size_t i = startIndex;
+	while (i < adapterJolts.size() && ((adapterJolts[i] - startJolt) <= 3))
+	{
+		//PDepth(depth);
+		//cout << " current i=" << i << " J=" << adapterJolts[i] << endl;
+
+		if (adapterJolts[i] != targetJolt)
+		{
+			depth++;
+			pathsHere += FindPaths(adapterJolts, i + 1, adapterJolts[i], targetJolt);
+			i++;
+			depth--;
+		}
+		else
+		{
+			//PDepth(depth);
+			//cout << " RETURNING 1 current i=" << i << " J=" << adapterJolts[i] << endl;
+			return 1;
+		}
+
+
+	}
+
+	//PDepth(depth);
+	//cout << " returning 0, current i=" << i << ", path here=" << pathsHere <<  endl;
+	return pathsHere;
+}
+
+
+unsigned long long int PartB(vector<string>& lines)
+{
+	vector<int> adapterJolts;
+	unsigned long long int result = 0;
+
+	PrepAdapters(lines, adapterJolts);
+
+	int targetJolt = adapterJolts.back();
+	int startJolt = 0;
+	int startIndex = 0;
+
+	result = FindPaths(adapterJolts, startIndex, startJolt, targetJolt);
+
+	return result;
+
 }
