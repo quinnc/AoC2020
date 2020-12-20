b4959187bf113b0071ac0927dc5119dd362cffa4
quinnc <q.liesch+git@gmail.com>
2020-12-20 15:23:16 -0700

    builds, running part A with example 1.txt

--- aoc2020/Day11/Day11.cpp
+++ aoc2020/Day11/Day11.cpp
@@ -43,9 +43,166 @@ int main(int argc, char** argv)
 }
 
 
+bool SeatTransform(const vector<string>& before, vector<string>& after)
+{
+	size_t xMax = before[0].length();
+	bool seatChanged = false;
+
+	for (size_t y = 0; y < before.size(); y++)
+	{
+		for (size_t x = 0; x < xMax; x++)
+		{
+			if (x == 0)
+				after.push_back(string());
+
+			if (before[y].at(x) == '.')
+			{
+				after[y].push_back('.');
+			}
+			else if (before[y].at(x) == 'L')
+			{
+				bool foundOccupied = false;
+				for (int yLook = -1; yLook <= 1; yLook++)
+				{
+					int currY = y + yLook;
+					if (currY < 0)
+						continue;
+
+					if (currY >= (int)before.size())
+						continue;
+
+					for (int xLook = -1; xLook <= 1; xLook++)
+					{
+						int currX = x + xLook;
+
+						if (currX < 0)
+							continue;
+
+						if (currY >= (int)xMax)
+							continue;
+
+						if (currX == 0 && currY == 0) // self
+							continue;
+
+
+						// if get here, then a valid location;
+						if (before[currY].at(currX) == '#') // occupied
+							foundOccupied = true;
+
+					}
+				}
+
+				if (!foundOccupied)
+				{
+					after[y].push_back('#');
+					seatChanged = true;;
+				}
+				else
+				{
+					after[y].push_back('L');
+				}
+
+			}
+			else if (before[y].at(x) == '#')
+			{
+				int occupiedCount = 0;
+
+				for (int yLook = -1; yLook <= 1; yLook++)
+				{
+					int currY = y + yLook;
+					if (currY < 0)
+						continue;
+
+					if (currY >= (int)before.size())
+						continue;
+
+					for (int xLook = -1; xLook <= 1; xLook++)
+					{
+						int currX = x + xLook;
+
+						if (currX < 0)
+							continue;
+
+						if (currY >= (int)xMax)
+							continue;
+
+						if (currX == 0 && currY == 0) // self
+							continue;
+
+
+						// if get here, then a valid location;
+						if (before[currY].at(currX) == '#') // occupied
+							occupiedCount++;
+
+					}
+				}
+
+				if (occupiedCount >= 4)
+				{
+					after[y].push_back('L');
+					seatChanged = true;;
+				}
+				else
+				{
+					after[y].push_back('#');
+				}
+			}
+		}
+	}
+
+	return seatChanged;
+}
+
+
+unsigned int CountOccupied(const vector<string>& init)
+{
+	size_t xMax = init[0].length();
+	unsigned int count = 0;
+
+	for (size_t y = 0; y < init.size(); y++)
+	{
+		for (size_t x = 0; x < xMax; x++)
+		{
+			if (init[y].at(x) == '#')
+				count++;
+		}
+	}
+
+	return count;
+}
+
 
 int PartA(vector<string>& lines)
 {
+	vector<string> init;
+	vector<string> next;
+
+	if (lines.size() < 3)
+	{
+		cout << " NOT ENOUGH LINES! " << endl;
+		return -1;
+	}
+
+	int rounds = 0;
+	bool transformDidChange = true;
+
+	std::copy(lines.begin(), lines.end(), init.begin());
+
+	while (rounds < 100 && transformDidChange)
+	{
+		transformDidChange = SeatTransform(init, next);
+
+		init.clear();
+		std::copy(next.begin(), next.end(), init.begin());
+		next.clear();
+		rounds++;
+	}
+
+	cout << "Rounds = " << rounds << endl;
+
+	cout << " Number occupied = " << CountOccupied(init) << endl;
+	
+
 	return 0;
 
 }
